c220f3b420cc55a1703b159bcd70586a8e1385c7
agile-madan-tamang <madan.tamang@agileitsolutions.net>
2016-12-31 15:18:41 +0545

    added week 2 file in git too

---
+++ week 2 - nodejs render html/server.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var fs = require('fs');
+
+function onRequest(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('./index.html', null, function(error, data) {
+        if (error) {
+            response.writeHead(404);
+            response.write('File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+}
+
+http.createServer(onRequest).listen(8000);
+console.log("server started localhost:800")
---
+++ week 3 - a nodejs render with routing/README.md
@@ -0,0 +1,6 @@
+# broadway-mean-stack
+
+a simple nodejs server with route url.
+
+basic command
+* node server 
